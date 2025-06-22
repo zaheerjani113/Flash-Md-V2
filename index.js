a1f1a7277ea103777db65a81ec9de574fd8b883b
France King <151246926+franceking1@users.noreply.github.com>
2025-06-22 16:21:55 +0300

    💆🤍 UPDATE

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('FLASH-MD V2.5 WhatsApp Bot is running!'));
+app.get('/', (req, res) => res.send('FLASH-MD V2.5  is running!'));
 app.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
 
 require('./db-init')();
